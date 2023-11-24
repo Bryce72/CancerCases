bc7d5883daba484e77ac73b2f8b65f660e9ef14d
Bryce72 <brycedunlapsf56@gmail.com>
2023-11-24 09:36:31 -0500

    Getting to know the code... Leukemias in florida

--- driver.py
+++ driver.py
@@ -1,1 +1,13 @@
-#cancer cases test
+import pandas as pd
+
+
+df = pd.read_csv('BYAREA_modified.txt', sep='|')
+print("Columns: ", df.columns)
+for row in df.itertuples():
+    if "Florida" in row:
+        if 'Leukemias' in row:
+            if 'Non-Hispanic White' in row:
+                if 'Mortality' in row:
+                    if '1999' in row:
+                        print(row)
+                    
